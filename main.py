4cf59fc0fc0202a403fc7d32fd3312b93b94c8db
Lucian-UCLM <lucianandrei.farcas@alu.uclm.es>
2022-11-29 13:52:32 -0800

    First commit

---
+++ main.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+sedes = []
+# --- Clase sede --- #
+class sede():
+    def __init__(self, sede, lat, lon):
+        self.sede = sede
+        self.lat = lat
+        self.lon = lon
+
+# --- calcularCoordenadas --- #
+def calcularCoordenadas(self):
+    coordenadas= ""
+
+    lat = 3 * sedes.
+    lon = 0
+
+    coordenadas = "{} , {}".format(lat,lon)
+
+    return coordenadas
+
+
+# --- MAIN --- #
+
+Leon = sede("León", 42.57682, -5.59698)
+Santiago = sede("Santiago de Compostela", 42.90510, -8.50701)
+Madrid = sede("Madrid" , 40.51642,  -3.89502)
+Barcelona = sede("Barcelona", 41.40124 , 2.19122)
+Sevilla = sede( "Sevilla",37.39523, -6.01029)
+
+sedes= [Leon, Santiago, Madrid, Barcelona, Sevilla]
+
+resultado = calcularCoordenadas(sedes=sedes)
+
+print("El molino defectuoso se encuentra en las coordenadas: {}", resultado)
